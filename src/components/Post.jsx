20c2769b22a9ac4473c1eb95c914d311fd945eb2
Shivani Tiwari <116451611+SheWe786@users.noreply.github.com>
2024-03-22 18:40:15 +0530

    Go-India-Intern-Assign

---
+++ src/components/Post.jsx
@@ -0,0 +1,49 @@
+import { FaRegHeart } from 'react-icons/fa';
+import { BiComment, BiShareAlt } from 'react-icons/bi';
+import { FiEye } from 'react-icons/fi';
+import { Card, UserProfile } from '.';
+import { convertToK, timeSince } from '@/utils/helper';
+import Image from 'next/image';
+
+const Post = ({ tag, time, user, post, actions }) => {
+  return (
+    <Card className='p-2 px-5 space-y-3'>
+      <header className='flex justify-end ml-auto text-sm font-semibold text-blue-500'>
+        {timeSince(time)}
+      </header>
+      <figcaption className='flex gap-2'>
+        <div className='w-[50px]'>
+          <UserProfile src={user.img} />
+        </div>
+        <div className='p-2 py-1 space-y-2 md:flex-grow'>
+          <h3 className='font-bold capitalize'>{user.name}</h3>
+
+          <p>{post.caption}</p>
+
+          <footer className='flex justify-between mt-1 text-sm font-semibold'>
+            <div className='post-btn'>
+              <button>
+                <FaRegHeart className='md:text-xl' />
+              </button>
+              {convertToK(actions.likes)}
+            </div>
+            <div className='post-btn'>
+              <FiEye className='md:text-xl' />
+              {convertToK(actions.views)}
+            </div>
+            <div className='post-btn'>
+              <BiComment className='md:text-xl' />
+              {convertToK(actions.comments)} Comments
+            </div>
+            <div className='post-btn'>
+              <BiShareAlt className='md:text-xl' />
+              Share
+            </div>
+          </footer>
+        </div>
+      </figcaption>
+    </Card>
+  );
+};
+
+export default Post;
---
+++ src/containers/Navbar.jsx
@@ -0,0 +1,30 @@
+import Image from 'next/image';
+import { Button, SearchBar, UserProfile } from '@/components';
+
+const Navbar = ({ className }) => {
+  return (
+    <nav
+      className={`flex items-center justify-between gap-10 p-2 px-5 bg-bg-secondary ${className}`}
+    >
+      <h1 className='text-lg'>
+        <a href='/'>
+          <Image src='/logo.webp' alt='logo' height={100} width={100} />
+        </a>
+      </h1>
+
+      <SearchBar className='flex-grow' />
+
+      <UserProfile
+        className='w-10 md:hidden'
+        src='https://source.unsplash.com/random/100x100?user,man,woman,employee'
+      />
+
+      {/* Buttons */}
+      <div className='hidden gap-2 md:flex '>
+        <Button>Contact Us</Button>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
